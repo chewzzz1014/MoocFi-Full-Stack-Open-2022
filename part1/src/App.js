bfba408f7e31a7eb21d4b2dd417fc9a8c78a0386
chewzzz <chewziqing@gmail.com>
2022-11-12 23:10:46 +0800

    Add new react components

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,12 +1,25 @@
+const Welcome = () => {
+  return (
+    <div>
+      <p><b>Welcome to React</b></p>
+    </div>
+  )
+}
+
 function App() {
   const now = new Date()
   const a = 10, b = 20
 
   return (
     <div>
+      <Welcome />
       <p>Hello world, it's {now.toString()}</p>
+
+      {/* every tag needs to be closed */}
+      <br />
+
       <p>
-        {a} plue {b} is {a + b}
+        {a} plus {b} is {a + b}
       </p>
     </div>
   );
