9c95475ba34ff8f3c0eafecadfbc9ef09823d26b
chewzzz <chewziqing@gmail.com>
2022-11-12 23:06:51 +0800

    Add contents to JSX

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const now = new Date()
+  const a = 10, b = 20
+
   return (
     <div>
-      <p>Hello World!</p>
+      <p>Hello world, it's {now.toString()}</p>
+      <p>
+        {a} plue {b} is {a + b}
+      </p>
     </div>
   );
 }
