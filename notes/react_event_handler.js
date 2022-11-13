d48d1f5004516b689ce3d16e1ff3c8def659d945
chewzzz <chewziqing@gmail.com>
2022-11-13 21:25:17 +0800

    Integrate useState with component

--- notes/react_event_handler.js
+++ notes/react_event_handler.js
@@ -1,16 +1,33 @@
 import { useState } from 'react'
 
 
+const Display = ({ counter }) => {
+    return (
+        <div>{counter}</div>
+    )
+}
+
+const Button = (props) => {
+    return (
+        <button onClick={props.onClick}>
+            {props.text}
+        </button>
+    )
+}
+
 const App = () => {
     const [counter, setCounter] = useState(0)
 
-    const handleClick = () => { console.log('clicked') }
+    const increaseByOne = () => setCounter(counter + 1)
+    const decreaseByOne = () => setCounter(counter - 1)
+    const setToZero = () => setCounter(0)
+
     return (
         <div>
-            <div>{counter}</div>
-            <button onClick={handleClick}>
-                plus
-            </button>
+            <Display counter={counter} />
+            <Button onClick={setToZero} text='zero' />
+            <Button onClick={increaseByOne} text='plus' />
+            <Button onClick={decreaseByOne} text='mi us' />
         </div>
     )
 }
