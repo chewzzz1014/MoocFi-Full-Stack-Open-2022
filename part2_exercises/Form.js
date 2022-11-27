6220d30a9e15667909e91eb74003678442ef989f
chewzzz <chewziqing@gmail.com>
2022-11-27 21:25:09 +0800

    Add form controlled component

---
+++ part2_exercises/Form.js
@@ -0,0 +1,26 @@
+const App = (props) => {
+    const [notes, setNotes] = useState(props.notes)
+    const [newNote, setNewNote] = useState(
+        'a new note...'
+    )
+
+    // ...
+
+    const handleNoteChange = (event) => { console.log(event.target.value)    setNewNote(event.target.value) }
+    return (
+        <div>
+            <h1>Notes</h1>
+            <ul>
+                {notes.map(note =>
+                    <Note key={note.id} note={note} />
+                )}
+            </ul>
+            <form onSubmit={addNote}>
+                <input
+                    value={newNote}
+                    onChange={handleNoteChange} />
+                <button type="submit">save</button>
+            </form>
+        </div>
+    )
+}
