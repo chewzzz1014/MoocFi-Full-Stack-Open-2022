770c5050a31f25506e33d05c44ca12e79bc709bb
chewzzz <chewziqing@gmail.com>
2022-12-13 23:01:59 +0800

    Add error handling

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -42,7 +42,7 @@ app.get("/info", (req, res) => {
     res.send(r)
 })
 
-app.get("/api/persons", async (req, res) => {
+app.get("/api/persons", async (req, res, next) => {
     try {
         const allPersons = await Person.find({})
         if (!allPersons || allPersons.length == 0) {
@@ -50,22 +50,23 @@ app.get("/api/persons", async (req, res) => {
         }
         res.json(allPersons)
     } catch (err) {
-        console.log(err)
+        next(err)
     }
 })
 
-app.get("/api/persons/:id", async (req, res) => {
+app.get("/api/persons/:id", async (req, res, next) => {
     const { id } = req.params
 
     try {
         const foundPerson = await Person.findById(mongoose.Types.ObjectId(id));
         res.json(foundPerson)
     } catch (err) {
-        res.status(404).send(`404: Contacts with ID ${id} Not Found!`)
+        // res.status(404).send(`404: Contacts with ID ${id} Not Found!`)
+        next(err)
     }
 })
 
-app.post("/api/persons", async (req, res) => {
+app.post("/api/persons", async (req, res, next) => {
     const body = req.body
 
     if (!body.name || !body.number) {
@@ -89,55 +90,73 @@ app.post("/api/persons", async (req, res) => {
         name: body.name,
         number: body.number
     })
-    await p.save()
-    console.log('Added new person!')
 
-    res.json({
-        operation: 'insertion',
-        status: 'success',
-        data: p
-    })
+    try {
+        await p.save()
+        console.log('Added new person!')
+
+        res.json({
+            operation: 'insertion',
+            status: 'success',
+            data: p
+        })
+    } catch (err) {
+        next(err)
+    }
 })
 
-app.put("/api/persons/:id", async (req, res) => {
+app.put("/api/persons/:id", async (req, res, next) => {
     const { id } = req.params
     const body = req.body
 
-    const updatedPerson = await Person.findOneAndUpdate({ _id: mongoose.Types.ObjectId(id) }, { number: body.number }, { new: true })
-    console.log(updatedPerson)
+    try {
+        const updatedPerson = await Person.findOneAndUpdate({ _id: mongoose.Types.ObjectId(id) }, { number: body.number }, { new: true })
+        console.log(updatedPerson)
 
-    res.json({
-        operation: 'update contacts',
-        status: 'success',
-        data: updatedPerson
-    })
+        res.json({
+            operation: 'update contacts',
+            status: 'success',
+            data: updatedPerson
+        })
+    } catch (err) {
+        next(err)
+    }
 })
 
-app.delete("/api/persons/:id", async (req, res) => {
+app.delete("/api/persons/:id", async (req, res, next) => {
     const { id } = req.params
-    const removedPerson = await Person.findOneAndDelete({ _id: id })
-    res.json({
-        operation: "deletion",
-        status: 'success',
-        data: removedPerson
-    })
+
+    try {
+        const removedPerson = await Person.findOneAndDelete({ _id: id })
+        res.json({
+            operation: "deletion",
+            status: 'success',
+            data: removedPerson
+        })
+    } catch (err) {
+        next(err)
+    }
 })
 
-app.delete("/api/persons/delete/:name", async (req, res) => {
+app.delete("/api/persons/delete/:name", async (req, res, next) => {
     const { name } = req.params
-    const result = await Person.deleteMany({ name: name })
-    res.json({
-        operation: 'deletions by name',
-        status: 'success',
-        result: result
-    })
+    try {
+        const result = await Person.deleteMany({ name: name })
+        res.json({
+            operation: 'deletions by name',
+            status: 'success',
+            result: result
+        })
+    } catch (err) {
+        next(err)
+    }
 })
 
 // error handling middleware
 app.use((err, req, res, next) => {
     console.log(err.message)
 
-    if (error.name === 'CastError') {
+    if (err.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
     }
     next(err)
