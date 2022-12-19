1eef15b7537888498824a60c0c51f3f8b783c599
chewzzz <chewziqing@gmail.com>
2022-12-19 15:56:56 +0800

    Add unit test for count sum of list

--- part4_submisson/blog/utils/list_helper.js
+++ part4_submisson/blog/utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    return blogs.reduce((sum, ele) => sum + ele)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
