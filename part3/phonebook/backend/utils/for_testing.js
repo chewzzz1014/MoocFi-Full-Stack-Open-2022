fa3d04fdeea436b5aa0511b679916da708aa6dab
chewzzz <chewziqing@gmail.com>
2022-12-19 15:47:15 +0800

    Add unit test for blog in part4

--- part3/phonebook/backend/utils/for_testing.js
+++ part3/phonebook/backend/utils/for_testing.js
@@ -10,7 +10,7 @@ const average = (arr) => {
         return sum + item
     }
 
-    return arr.length === 0 ? 0 : arr.reducer(reducer, 0) / arr.length
+    return arr.length === 0 ? 0 : arr.reduce(reducer, 0) / arr.length
 }
 
 module.exports = {
