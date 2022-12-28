4f0bad2f0df36928ecdd97ecdf51f1c4809abc8e
chewzzz <chewziqing@gmail.com>
2022-12-28 14:44:15 +0800

    Add restrictions on username and password

--- part4_submisson/blog/routes/users.js
+++ part4_submisson/blog/routes/users.js
@@ -18,6 +18,12 @@ router.post('/', async (req, res) => {
         })
     }
 
+    if (username.length < 3 || password.length < 3) {
+        return res.status(400).json({
+            error: 'username and password must contain at least 3 characters'
+        })
+    }
+
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(password, saltRounds)
 
