6ccdbc098f4c52eed34e85de53d01a3cb33bff35
chewzzz <chewziqing@gmail.com>
2023-01-03 11:26:34 +0800

    Add setToken method in notes services

--- part5_exercises/part2-notes-part5-1/src/services/notes.js
+++ part5_exercises/part2-notes-part5-1/src/services/notes.js
@@ -3,15 +3,25 @@ import axios from 'axios'
 // we've set the proxy as localhost:3001
 const baseUrl = '/api/notes'
 
+let token = null
+
+const setToken = newToken => {
+  token = `bearer ${newToken}`
+}
+
 const getAll = () => {
   const request = axios.get(baseUrl)
 
   return request.then(response => response.data)
 }
 
-const create = newObject => {
-  const request = axios.post(baseUrl, newObject)
-  return request.then(response => response.data)
+const create = async newObject => {
+  const config = {
+    headers: { Authorization: token }
+  }
+
+  const request = await axios.post(baseUrl, newObject, config)
+  return request.data
 }
 
 const update = (id, newObject) => {
@@ -19,4 +29,4 @@ const update = (id, newObject) => {
   return request.then(response => response.data)
 }
 
-export default { getAll, create, update }
+export default { getAll, create, update, setToken }
