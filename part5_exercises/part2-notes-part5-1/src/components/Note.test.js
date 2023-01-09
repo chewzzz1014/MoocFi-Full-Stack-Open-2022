f4381615fe4d7432f87b29bec0eebeefa33b8587
chewzzz <chewziqing@gmail.com>
2023-01-09 16:38:50 +0800

    Fix: type in test getByText

--- part5_exercises/part2-notes-part5-1/src/components/Note.test.js
+++ part5_exercises/part2-notes-part5-1/src/components/Note.test.js
@@ -40,7 +40,7 @@ test('clicking the button calls event handler once', async () => {
 
     // session started to interact with rendered component
     const user = userEvent.setup()
-    const button = screen.getByText('make no important', { exact: false })
+    const button = screen.getByText('make not important', { exact: false })
     await user.click(button)
 
     expect(mockHandler.mock.calls).toHaveLength(1)
