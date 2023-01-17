42d2167220159a412d2bde963c7dcda09771fa95
chewzzz <chewziqing@gmail.com>
2023-01-17 08:52:12 +0800

    Add beforeeach block in test that visit the client

--- part5/bloglist-frontend-main/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend-main/cypress/e2e/blog_app.cy.js
@@ -1,13 +1,15 @@
 describe('Blog app', function () {
-    it('front page can be opened', function () {
+    beforeEach(function () {
         cy.visit('http://localhost:3000')
+    })
+
+    it('front page can be opened', function () {
         cy.contains('username')
         cy.contains('password')
         cy.contains('Login to Application')
     })
 
-    // it('login form can be opened', function () {
-    //     cy.visit('http://localhost:3000')
-    //     cy.contains('login').click()
-    // })
+    it('login form can be opened', function () {
+        cy.contains('login').click()
+    })
 })
