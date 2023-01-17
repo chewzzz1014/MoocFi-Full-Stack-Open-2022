dd0ee1d8d6b50ebb1d46722f0e6ae06c746534fa
chewzzz <chewziqing@gmail.com>
2023-01-17 08:32:19 +0800

    Create test for note app; Rename test for blog app

--- part5/bloglist-frontend-main/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend-main/cypress/e2e/blog_app.cy.js
@@ -1,7 +1,8 @@
 describe('Blog app', function () {
     it('front page can be opened', function () {
         cy.visit('http://localhost:3000/')
-        cy.contains('Blogs')
+        cy.contains('username')
+        cy.contains('password')
         cy.contains('Login to Application')
     })
 })
