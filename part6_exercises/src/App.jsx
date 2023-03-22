e1689f83adac2e8d60ae21069f6bfcc29983a6a0
chewzzz <chewziqing@gmail.com>
2023-03-22 20:11:54 +0800

    add btn that dispatch action

--- part6_exercises/src/App.jsx
+++ part6_exercises/src/App.jsx
@@ -1,6 +1,29 @@
-import './App.css'
+import store from './store/store'
 
 function App() {
+    return (
+        <div>
+            <div>{store.getState()}</div>
+
+            <button
+                onClick={() => store.dispatch({ type: 'INCREMENT' })}
+            >
+                Plus
+            </button>
+
+            <button
+                onClick={() => store.dispatch({ type: 'DECREMENT' })}
+            >
+                Minus
+            </button>
+
+            <button
+                onClick={() => store.dispatch({ type: 'ZERO' })}
+            >
+                Zero
+            </button>
+        </div>
+    )
 }
 
 export default App
