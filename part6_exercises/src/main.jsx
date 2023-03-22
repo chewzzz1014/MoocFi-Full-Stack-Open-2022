df8658723d98976e1e2700a5781bfad4cc49389d
chewzzz <chewziqing@gmail.com>
2023-03-22 20:19:13 +0800

    add store subscriber callback func to render App

--- part6_exercises/src/main.jsx
+++ part6_exercises/src/main.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import store from './store/store'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// to rerender App when state is changed
+const renderApp = () => {
+  root.render(<App />)
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+renderApp()
+// everytime state is changed, render App component
+store.subscribe(renderApp)
