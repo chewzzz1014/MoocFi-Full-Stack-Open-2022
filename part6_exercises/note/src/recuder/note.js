5da9b8b6921a047486bb1c94115ffabd4ab47336
chewzzz <chewziqing@gmail.com>
2023-03-22 20:58:37 +0800

    add comment

--- part6_exercises/note/src/recuder/note.js
+++ part6_exercises/note/src/recuder/note.js
@@ -1,3 +1,6 @@
+// reducer must be pure function
+// pure function: do not cause any side effects and must return the same response when called with the same parameters
+
 const noteReducer = (state = [], action) => {
     switch (action.type) {
         case 'NEW_NOTE':
