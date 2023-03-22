684bcbabb4b99e9554174215df9ed2bcbc69757e
chewzzz <chewziqing@gmail.com>
2023-03-22 22:05:06 +0800

    add form; dispatch new_note on submit

--- part6_exercises/note/src/main.jsx
+++ part6_exercises/note/src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import store from './store/store'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const renderApp = () => {
+  root.render(<App />)
+}
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+
+renderApp()
+store.subscribe(renderApp)
