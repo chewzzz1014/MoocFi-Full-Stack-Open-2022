3911edafd8f529feab9d35353bdd56956ba209a7
chewzzz <chewziqing@gmail.com>
2023-03-22 22:16:51 +0800

    add provider

--- part6_exercises/note/src/main.jsx
+++ part6_exercises/note/src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import store from './store/store'
+import { Provider } from 'react-redux'
 
 const renderApp = () => {
-  root.render(<App />)
+  root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
 }
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
