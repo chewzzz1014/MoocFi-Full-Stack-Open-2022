b50bcbd1bc82a0bb7c4ad31415bebfcf25ebac6b
chewzzz <chewziqing@gmail.com>
2023-03-22 20:50:19 +0800

    add store

---
+++ part6_exercises/note/src/store/store.js
@@ -0,0 +1,22 @@
+import { createStore } from 'redux'
+import { noteReducer } from '../recuder/note'
+
+const store = createStore(noteReducer)
+
+store.dispatch({
+    type: 'NEW_NOTE',
+    payload: {
+        content: 'the app is in redux store',
+        important: true,
+        id: 1
+    }
+})
+
+store.dispatch({
+    type: 'NEW_NOTE',
+    payload: {
+        content: 'state changes are made with actions',
+        important: false,
+        id: 2
+    }
+})
