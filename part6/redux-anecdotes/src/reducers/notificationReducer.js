2e860d1273bef56e1293821e670e5bd95a1f84ec
chewzzz <chewziqing@gmail.com>
2023-03-30 13:31:23 +0800

    Add notificationSlice

---
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+]
+
+const notificationSlice = createSlice({
+    name: 'notifications',
+    initialState,
+    reducers: {
+        addNotification(state, action) {
+            state.push(action.content)
+        }
+    }
+})
+
+// export action creators
+export const { addNotification } = notificationSlice.actions
+// export reducer 
+export default notificationSlice.reducer
