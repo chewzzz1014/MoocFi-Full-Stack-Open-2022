363db537af74da33ab76bf95b61cd1af7b4ab72e
chewzzz <chewziqing@gmail.com>
2023-03-30 20:56:59 +0800

    Add notification that disappear afther 3 seconds

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,12 +1,18 @@
+import { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { voteAction, addAction } from './actions/vote'
+import { addNotification, removeNotification } from './reducers/notificationReducer'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
+import Notification from './components/Notification'
 
 const App = () => {
   const filter = useSelector(state => state.filter)
   let anecdotes = useSelector(state => state.anecdotes)
+  let notifications = useSelector(state => state.notifications)
+
+  const [hasNotification, setHasNotification] = useState(false)
 
   anecdotes = anecdotes.filter(a =>
     a.content.toLowerCase().includes(filter.toLowerCase())
@@ -21,10 +27,18 @@ const App = () => {
   const addAnec = (e) => {
     e.preventDefault()
     dispatch(addAction(e.target.contentField.value))
+    dispatch(addNotification(e.target.contentField.value))
+
+    setHasNotification(true)
+    setTimeout(() => {
+      dispatch(removeNotification(e.target.contentField.value))
+      setHasNotification(false)
+    }, 3000)
   }
 
   return (
     <div>
+      {hasNotification && <Notification />}
       <h2>Anecdotes</h2>
       <Filter />
       <AnecdoteList anecdotes={anecdotes} vote={vote} />
--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,16 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-]
-
 const notificationSlice = createSlice({
     name: 'notifications',
-    initialState,
+    initialState: [],
     reducers: {
         addNotification(state, action) {
-            state.push(action.payload)
+            state.push(`added ${action.payload}`)
         },
         removeNotification(state, action) {
             state.filter(n => n !== action.payload)
@@ -19,6 +14,6 @@ const notificationSlice = createSlice({
 })
 
 // export action creators
-export const { addNotification } = notificationSlice.actions
+export const { addNotification, removeNotification } = notificationSlice.actions
 // export reducer 
 export default notificationSlice.reducer
