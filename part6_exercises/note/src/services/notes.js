a2a0ef724e1f78e280ec0639ba81feec2fe08caa
chewzzz <chewziqing@gmail.com>
2023-03-31 15:10:45 +0800

    add services/notes.js

---
+++ part6_exercises/note/src/services/notes.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const BASE_URL = 'http://localhost:3001/notes'
+
+const getAll = async () => {
+    const response = await axios.get(BASE_URL)
+    return response.data
+}
+
+export default { getAll }
