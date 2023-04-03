6e25112cbcd86f8d15d69a602b2aa4239d3226dd
chewzzz <chewziqing@gmail.com>
2023-04-03 19:48:30 +0800

    synchronize data to server using react query

--- part6_exercises/query-notes/src/App.js
+++ part6_exercises/query-notes/src/App.js
@@ -1,21 +1,44 @@
-import {useQuery} from 'react-query'
-import { getNotes } from './requests'
+import {useQuery, useMutation, useQueryClient} from 'react-query'
+import { getNotes, createNote, updateNote } from './requests'
 
 // use react query to manage state
 // do not need useState and useEffect anymore!
 
 const App = () => {
+  const queryClient = useQueryClient()
+
+  // do something after mutation is executed
+  // tell react query that old result of the query whose key is the string notes should be invalidated
+  const newNoteMutation = useMutation(createNote, {
+    onSuccess: () => {
+      // React Query will automatically update a query with the key notes, i.e. fetch the notes from the server.
+      // As a result, the application renders the up-to-date state on the server, i.e. the added note is also rendered.
+      queryClient.invalidateQueries('notes')
+    }
+  })
+
+  const updateNoteMutation = useMutation(updateNote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('notes')
+    }
+  })
+
   const addNote = async (event) => {
     event.preventDefault()
     const content = event.target.note.value
     event.target.note.value = ''
-    console.log(content)
+    // to mutate the state
+    newNoteMutation.mutate({content, important: true})
   }
 
   const toggleImportance = (note) => {
-    console.log('toggle importance of', note.id)
+    updateNoteMutation.mutate({...note, important: !note.important})
   }
 
+  // useQuery:
+  // first params: string acts as key to query
+  // second params: function
+  // return: object that indicates the status of the query
   const result = useQuery(
     'notes', getNotes)
   console.log(result)
--- part6_exercises/query-notes/src/requests.js
+++ part6_exercises/query-notes/src/requests.js
@@ -5,3 +5,11 @@ const BASE_URL = 'http://localhost:3001/notes'
 export const getNotes = () => {
     return axios.get(BASE_URL).then(res => res.data)
 }
+
+export const createNote = (newNote) => {
+    return axios.post(BASE_URL, newNote).then(res => res.data)
+}
+
+export const updateNote = (updatedNote) => {
+    return axios.put(`${BASE_URL}/${updatedNote.id}`, updatedNote).then(res => res.data)
+}
