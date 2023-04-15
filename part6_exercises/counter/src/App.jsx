deceb4bfe54c0c9aa82c832f63ffb6e7d07f85d6
chewzzz <chewziqing@gmail.com>
2023-04-15 20:42:48 +0800

    Refactor App.jsx

--- part6_exercises/counter/src/App.jsx
+++ part6_exercises/counter/src/App.jsx
@@ -1,41 +1,15 @@
-import {useContext} from 'react'
-import CounterContext from './CounterContext'
-
-const Button = ({type, label}) => {
-    const [counter, dispatch] = useContext(CounterContext)
-    return (
-        <button onClick={() => dispatch({type})}>
-            {label}
-        </button>
-    )
-}
-
-const counterReducer = (state, action) => {
-    switch(action.type){
-        case "INC":
-            return state + 1
-        case "DEC":
-            return state - 1
-        case "ZERO":
-            return 0
-        default:
-            return state
-    }
-}
+import Display from './components/Display'
+import Button from './components/Button'
 
 const App = () => {
-    const [counter, counterDispatch] = useReducer(counterReducer, 0)
-
     return (
         <div>
-            <CounterContext.Provider value={[counter,  counterDispatch]}>
-                <Display counter={counter} />
-                <div>
-                    <Button type='INC' label='+' />
-                    <Button type='DEC' label='-' />
-                    <Button type='ZERO' label='0' />
-                </div>
-            </CounterContext.Provider>
+            <Display />
+            <div>
+                <Button type='INC' label='+' />
+                <Button type='DEC' label='-' />
+                <Button type='ZERO' label='0' />
+            </div>
         </div>
     )
 }
