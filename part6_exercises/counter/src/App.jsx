3b9ed4e913c1617387c248227d13aa1f72b3d158
chewzzz <chewziqing@gmail.com>
2023-04-15 20:42:11 +0800

    Move Button component to new Button.jsx

--- part6_exercises/counter/src/App.jsx
+++ part6_exercises/counter/src/App.jsx
@@ -1,11 +1,6 @@
 import {useContext} from 'react'
 import CounterContext from './CounterContext'
 
-const Display = ({counter}) => {
-    const [counter, dispatch] = useContext(CounterContext)
-    return <div>{counter}</div>
-}
-
 const Button = ({type, label}) => {
     const [counter, dispatch] = useContext(CounterContext)
     return (
