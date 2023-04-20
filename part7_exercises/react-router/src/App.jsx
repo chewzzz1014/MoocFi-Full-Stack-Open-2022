4143b0577c374ceb6f9fd6b4619c016655235fde
chewzzz <chewziqing@gmail.com>
2023-04-20 18:58:51 +0800

    add App component

--- part7_exercises/react-router/src/App.jsx
+++ part7_exercises/react-router/src/App.jsx
@@ -1,39 +1,72 @@
 import {
   BrowserRouter as Router,
-  Routes, Route, Link 
+  Routes, Route, Link, Navigate
 } from 'react-router-dom'
-
-const Home = () => (
-  <div> <h2>TKTL notes app</h2> </div>
-)
-
-const Notes = () => (
-  <div> <h2>Notes</h2> </div>
-)
-
-const Users = () => (
-  <div> <h2>Users</h2> </div>
-)
+import { useState } from 'react'
+import Notes from './components/Notes'
+import Note from './components/Note'
+import Users from './components/Users'
+import Login from './components/Login'
+import Home from './components/Home'
 
 function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      content: 'HTML is easy',
+      important: true,
+      user: 'Matti Luukkainen'
+    },
+    {
+      id: 2,
+      content: 'Browser can execute only JavaScript',
+      important: false,
+      user: 'Matti Luukkainen'
+    },
+    {
+      id: 3,
+      content: 'Most important methods of HTTP-protocol are GET and POST',
+      important: true,
+      user: 'Arto Hellas'
+    }
+  ])
+
+  const [user, setUser] = useState(null)
+
+  const login = (user) => {
+    setUser(user)
+  }
+
   const padding = {
     padding: 5
   }
 
   return (
-    <Router>
-      <div>
-        <Link style={padding} to='/'>home</Link>
-        <Link style={padding} to='/notes'>notes</Link>
-        <Link style={padding} to='/users'>users</Link>
-      </div>
+    <div>
+      <Router>
+        <div>
+          <Link style={padding} to='/'>home</Link>
+          <Link style={padding} to='/notes'>notes</Link>
+          <Link style={padding} to='/users'>users</Link>
+          {user
+            ? <em>{user} logged in</em>
+            : <Link style={padding} to="/login">login</Link>
+          }
+        </div>
 
-      <Routes>
-        <Route path='/notes' element={<Notes />} />
-        <Route path='/users' element={<Users />} />
-        <Route path='/' element={<Home />} />
-      </Routes>
-    </Router>
+        <Routes>
+          <Route path="/notes/:id" element={<Note notes={notes} />} />
+          <Route path="/notes" element={<Notes notes={notes} />} />
+          <Route path="/users" element={user ? <Users /> : <Navigate replace to="/login" />} />
+          <Route path="/login" element={<Login onLogin={login} />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+      <div>
+        <br />
+        <em>Note app, Department of Computer Science 2023</em>
+      </div>
+    </div>
   )
 }
 
