d31f9d318c208d9add7334a22f4e1f8e54fd9ede
chewzzz <chewziqing@gmail.com>
2023-05-01 14:36:56 +0800

    Complete exercise 7.8

--- part7/ultimate-hooks/src/App.js
+++ part7/ultimate-hooks/src/App.js
@@ -18,10 +18,18 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  const fetchData = async() => {
+    const response = await axios.get(baseUrl)
+    setResources(response.data) 
+  }
 
-  const create = (resource) => {
-    // ...
+  useEffect(() => {
+    fetchData()  
+  }, [])
+
+  const create = async (resource) => {
+    await axios.post(baseUrl, resource)
+    setResources([...resources, resource])
   }
 
   const service = {
