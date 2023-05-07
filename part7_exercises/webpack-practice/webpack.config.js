6b197143928458765e71872cf23e84ea86ed41e5
chewzzz <chewziqing@gmail.com>
2023-05-07 15:43:30 +0800

    setup loader; install babel

--- part7_exercises/webpack-practice/webpack.config.js
+++ part7_exercises/webpack-practice/webpack.config.js
@@ -7,6 +7,18 @@ const config = () => {
         output: {
             path: path.resolve(__dirname, 'build'),
             filename: 'main.js'
+        },
+        // loaders to inform webpack of files need to be processed before they're bundled
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react']
+                    }
+                }
+            ]
         }
     }
 }
