4d413e6a03976b7404026634023303d7195a30c9
chewzzz <chewziqing@gmail.com>
2023-05-08 19:12:48 +0800

    import core-js and regenerator-runtime

--- part7_exercises/webpack-practice/src/index.js
+++ part7_exercises/webpack-practice/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+// for rendering async/await on browsers that're not rendering them
+import 'core-js/stable/index.js'
+import "regenerator-runtime/runtime.js"
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
