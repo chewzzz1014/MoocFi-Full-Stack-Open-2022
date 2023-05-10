554d384854fd682edfed7ab927ddafe8b12b0345
chewzzz <chewziqing@gmail.com>
2023-05-10 14:04:02 +0800

    install css and style loader; fix typo

--- part7_exercises/webpack-practice/webpack.config.js
+++ part7_exercises/webpack-practice/webpack.config.js
@@ -21,7 +21,7 @@ const config = () => {
                     test: /\.js$/,
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-react', '@babel/preset-react']
+                        presets: ['@babel/preset-env', '@babel/preset-react']
                     }
                 },
                 {
