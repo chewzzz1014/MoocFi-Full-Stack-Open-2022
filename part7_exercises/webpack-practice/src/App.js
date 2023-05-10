e6aef9f8cef78de21b776ab72b3aa9fb7132c1b1
chewzzz <chewziqing@gmail.com>
2023-05-10 14:07:53 +0800

    add counter

--- part7_exercises/webpack-practice/src/App.js
+++ part7_exercises/webpack-practice/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './index.css'
 
 function App() {
+  const [counter, setCounter] = useState(0)
+  const [values, setValues] = useState([])
+
+  const handleClick = () => {
+    setCounter(counter+1)
+    setValues(values.concat(counter))
+  }
+
   return (
     <div className='container'>
-      hello webapck
+      hello webpack {counter} clicks
+      <button onClick={handleClick}>
+        press
+      </button>
     </div>
   )
 }
