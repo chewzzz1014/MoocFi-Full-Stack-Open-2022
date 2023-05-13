691e7249c26e8e97989d3947b6edc64c10449391
chewzzz <chewziqing@gmail.com>
2023-05-13 14:43:57 +0800

    add useNotes hook; change dev server port

--- part7_exercises/webpack-practice/src/App.js
+++ part7_exercises/webpack-practice/src/App.js
@@ -1,9 +1,24 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './index.css'
+import axios from 'axios'
+
+// custom hook
+// refetch data when url is changed (dev/prod)
+const useNotes = (url) => {
+  const [notes, setNotes] = useState([])
+  useEffect(() => {
+    axios.get(url).then(response => {
+      setNotes(response.data)
+    })
+  }, [url])
+  return notes
+}
 
 function App() {
   const [counter, setCounter] = useState(0)
   const [values, setValues] = useState([])
+  const url = 'https://notes2023.fly.dev/api/notes'
+  const notes = useNotes(url)
 
   const handleClick = () => {
     setCounter(counter+1)
@@ -13,9 +28,8 @@ function App() {
   return (
     <div className='container'>
       hello webpack {counter} clicks
-      <button onClick={handleClick}>
-        press
-      </button>
+      <button onClick={handleClick}>press</button>
+      <div>{notes.length} notes on server</div>
     </div>
   )
 }
