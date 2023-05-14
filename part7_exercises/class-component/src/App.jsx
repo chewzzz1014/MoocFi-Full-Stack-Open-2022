c753276c1e095af6f83eee6af57dd9b46714235e
chewzzz <chewziqing@gmail.com>
2023-05-14 15:37:56 +0800

    add lifecycle method and using this.setState

--- part7_exercises/class-component/src/App.jsx
+++ part7_exercises/class-component/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 
 // React class component
 class App extends React.Component {
@@ -11,6 +12,24 @@ class App extends React.Component {
       anecdotes: [],
       current: 0
     }
+  }
+
+  // lifecycle method
+  // executed once right after the first time component renders
+  componentDidMount = () => {
+    axios.get('http://localhost:3001/anecdotes')
+      .then((response) => {
+        // only updates 1 state
+        this.setState({
+          anecdotes: response.data
+        })
+      })
+  }
+
+  handleClick = () => {
+    // jumps to random anecdote
+    const current = Math.floor(Math.random()  * this.state.anecdotes.length)
+    this.setState({current})
   }
 
   render() {
@@ -26,7 +45,7 @@ class App extends React.Component {
         <div>
           {this.state.anecdotes[this.state.current].content}
         </div>
-        <button>next</button>
+        <button onClick={this.handleClick}>next</button>
       </div>
     )
   }
