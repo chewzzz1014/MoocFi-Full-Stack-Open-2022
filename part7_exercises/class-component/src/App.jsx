e287b3cfc4aec9171c7bd67d0f5003351ef63921
chewzzz <chewziqing@gmail.com>
2023-05-14 15:27:45 +0800

    add react class component

--- part7_exercises/class-component/src/App.jsx
+++ part7_exercises/class-component/src/App.jsx
@@ -1,9 +1,35 @@
-function App() {
-  return (
-    <div>
+import React from 'react'
 
-    </div>
-  )
+// React class component
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    // all states are stored as properties of this.state
+    // initialize this.state
+    this.state = {
+      anecdotes: [],
+      current: 0
+    }
+  }
+
+  render() {
+    // if no anecdote render this
+    if (this.state.anecdotes.length === 0) {
+      return <div>no anecdotes...</div>
+    }
+
+    // else, render this
+    return (
+      <div>
+        <h1>anecdote of the day</h1>
+        <div>
+          {this.state.anecdotes[this.state.current].content}
+        </div>
+        <button>next</button>
+      </div>
+    )
+  }
 }
 
 export default App
