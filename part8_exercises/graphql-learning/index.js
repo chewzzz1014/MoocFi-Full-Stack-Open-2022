a4700d21483c90c5bc9fedcbe0e8a9e98fca059b
chewzzz <chewziqing@gmail.com>
2023-05-31 19:40:13 +0800

    graphql enum

--- part8_exercises/graphql-learning/index.js
+++ part8_exercises/graphql-learning/index.js
@@ -41,9 +41,14 @@ const typeDefs = `
         id: ID!
     }
 
+    enum YesNo {
+        YES
+        NO
+    }
+
     type Query {
         personCount: Int!
-        allPersons: [Person!]!
+        allPersons(phone: YesNo): [Person!]!
         findPerson(name: String!): Person
     }
 
@@ -73,7 +78,15 @@ const typeDefs = `
 const resolvers = {
     Query: {
         personCount: () => persons.length,
-        allPersons: () => persons,
+        allPersons: (root, args) => {
+            if (!args.phone) {
+                return persons
+            }
+
+            const byPhone = (person) => 
+                args.phone === 'YES' ? person.phone : !person.phone
+            return persons.filter(byPhone)
+        },
         findPerson: (root, args) => 
             persons.find(p => p.name === args.name)
     },
