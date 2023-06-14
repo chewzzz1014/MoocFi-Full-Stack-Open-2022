46e31878d9724e126760ef2ea5f9c14433b52009
chewzzz <chewziqing@gmail.com>
2023-06-14 14:47:14 +0800

    add gql query

--- part8_exercises/graphql-phonebook-frontend/src/index.js
+++ part8_exercises/graphql-phonebook-frontend/src/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {ApolloClient, InMemoryCache, gql} from '@apollo/client'
+
+const client = new ApolloClient({
+  url: 'https://localhost:4000',
+  cache: new InMemoryCache(),
+})
+
+const query = gql`
+  query{
+    allPersons {
+      name,
+      phone,
+      address {
+        street,
+        city
+      }
+      id
+    }
+  }
+`
+
+client.query({query})
+  .then((response) => {
+    console.log(response.data)
+  })
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
