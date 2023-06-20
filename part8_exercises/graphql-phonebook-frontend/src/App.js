e580c38230f03d8f749f741ce4c269dbf4da3be4
chewzzz <chewziqing@gmail.com>
2023-06-20 20:36:27 +0800

    handling mutation error

--- part8_exercises/graphql-phonebook-frontend/src/App.js
+++ part8_exercises/graphql-phonebook-frontend/src/App.js
@@ -2,8 +2,12 @@ import {useQuery} from '@apollo/client'
 import { ALL_PERSONS } from './queries'
 import Persons from './components/Persons';
 import PersonForm from './components/PersonForm';
+import { useState } from 'react';
 
 function App() {
+  // for handling mutation errors in PersonForm
+  const [errorMessage, setErrorMessage] = useState(null)
+
   // make queries
   const result = useQuery(ALL_PERSONS)
 
@@ -11,12 +15,30 @@ function App() {
     return <div>loading...</div>
   }
 
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
   return (
     <div>
-      <Persons  persons={result.data.allPersons}/>
-      <PersonForm />
+      
+      <Persons persons={result.data.allPersons}/>
+      <PersonForm setError={notify}/>
     </div>
   );
+}
+
+const Notify = ({errorMessage}) => {
+  if (!errorMessage)
+    return null 
+  return (
+    <div style={{color: 'red'}}>
+      {errorMessage}
+    </div>
+  )
 }
 
 export default App;
