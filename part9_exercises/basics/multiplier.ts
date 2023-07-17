3703e1a6b547a1f50d28ea0f7f8535a252534a67
chewzzz <chewziqing@gmail.com>
2023-07-17 16:38:33 +0800

    add parseArguments method

--- part9_exercises/basics/multiplier.ts
+++ part9_exercises/basics/multiplier.ts
@@ -1,3 +1,26 @@
+// define type
+interface MultiplyValues {
+    value1: number,
+    value2: number
+}
+
+// parse 2 values from cli
+const parseArguments = (args: String[]): MultiplyValues => {
+    if (args.length < 4)
+        throw new Error('Not enought arguments')
+    if (args.length > 4)
+        throw new Error('Too many arguments')
+
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            value1: Number(args[2]),
+            value2: Number(args[3])
+        }
+    } else {
+        throw new Error('Provided values were not numbers!')
+    }
+}
+
 const multiplicator = (a: number, b: number, printText: string) => {
     console.log(printText, a * b)
 }
