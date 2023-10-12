a3dd9d1288af7d5001ae1473a4d7187913645985
chewzzz <chewziqing@gmail.com>
2023-10-12 22:34:34 +0800

    react ts note app

--- part9_react_ts/src/App.tsx
+++ part9_react_ts/src/App.tsx
@@ -1,11 +1,49 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Note } from './types'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // use type parameter
+  const [newNote, setNewNote] = useState('')
+  const [notes, setNotes] = useState<Note[]>([
+    {id: 1, content: 'testing'}
+  ])
+
+  useEffect(() => {
+    // specify response data type
+    axios.get('http://localhost:3001/notes')
+      .then((response) => {
+        console.log(response.data)
+        setNotes(response.data as Note[])
+      })
+  }, [])
+
+  const noteCreation = (e: React.SyntheticEvent) => {
+    e.preventDefault()
+
+    const noteToAdd = {
+      content: newNote,
+      id: notes.length + 1
+    }
+    setNotes(notes.concat(noteToAdd))
+    setNewNote('')
+  }
 
   return (
-    <>
-    </>
+    <div>
+      <form onSubmit={noteCreation}>
+        <input
+          value={newNote}
+          onChange={(e) => setNewNote(e.target.value)}
+        />
+        <button type="submit">add</button>
+      </form>
+      <ul>
+        {notes.map(n => (
+          <li key={n.id}>{n.content}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
--- part9_react_ts/src/main.tsx
+++ part9_react_ts/src/main.tsx
@@ -1,14 +1,7 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App'
 
-interface WelcomeProps {
-  name: string
-}
-
-const Welcome = (props: WelcomeProps) => {
-  return <h1>Hello, {props.name}</h1>
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Welcome name='Sarah' />
+  <App />
 )
