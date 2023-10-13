4e11230c462e4735be65526a03c9e199ee7c7492
chewzzz <chewziqing@gmail.com>
2023-10-13 19:00:00 +0800

    define types for entry

--- part9_patientor/backend/src/types.ts
+++ part9_patientor/backend/src/types.ts
@@ -10,10 +10,6 @@ export enum Gender {
     Other = 'other'
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {   
-}
-
 export interface Patient {
     id: string;
     name: string;
@@ -25,3 +21,53 @@ export interface Patient {
 }
 
 export type ProtectedPatient = Omit<Patient, 'ssn' | 'entries'>
+
+interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+// Hospital Entry
+interface Discharge {
+    date: string;
+    criteria: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+    type: 'Hospital';
+    discharge: Discharge;
+}
+
+// Occupational Healthcare Entry
+interface SickLeave {
+    startDate: string;
+    endDate: string;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+    type: 'OccupationalHealthcare';
+    employerName: string;
+    sickLeave?: SickLeave;
+}
+
+// Healthcheck Entry
+export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+    type: 'HealthCheck';
+    healthCheckRating: HealthCheckRating;
+}
+
+export type Entry = HospitalEntry | OccupationalHealthcareEntry | HealthCheckEntry
+
+// use Omit to exclude a property (for union type): resulting type would only contain the common properties
+type UnionOmit<T, K extends string| number | symbol> = T extends unknown ? Omit<T, K> : never // Define special omit for unions
+export type EntryWithoutId = UnionOmit<FileSystemEntry, 'id'>
